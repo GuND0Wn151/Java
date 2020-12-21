464102aac88ba17826323fad61aee511dcea3c05
GuND0Wn151 <kmaheshgupta151@outlook.com>
2020-12-21 17:22:20 +0530

    Create 11_StringOps.java

---
+++ 11_StringOps.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Task13{
+	public static void main(String args[]){
+		String a="This is a String";
+		Scanner input = new Scanner(System.in);
+		System.out.println("charAt:"+a.charAt(2));
+		System.out.println("indexOf:"+a.indexOf("s"));
+		System.out.println("lastindex:"+a.lastIndexOf("i"));
+		System.out.println("length:"+a.length());
+		System.out.println("subString:"+a.substring(2,5));
+		System.out.println("compareTo"+a.compareTo("hi"));
+		System.out.println("equals"+a.equals("time"));
+		//System.out.println();
+	}
+}
