662024c76b7ad101dbd30a30be7f76c1e58dfb34
GuND0Wn151 <kmaheshgupta151@outlook.com>
2020-12-21 17:22:17 +0530

    Create 10_Sort.java

---
+++ 10_Sort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Task11{
+	public static void main(String args[]){
+		int temp,min=0;
+		int[] ar=new int[10];
+		Scanner input=new Scanner(System.in);
+		System.out.println("ENter the values of array");
+		for(int i=0;i<10;i++)
+		ar[i]=input.nextInt();
+		for(int i=0;i<10;i++){
+			min=i;
+			for(int j=i+1;j<10;j++){
+				if(ar[i]>ar[j])
+				min=j;
+			}
+			temp=ar[min];
+			ar[min]=ar[i];
+			ar[i]=temp;
+			
+		}
+		for(int i=0;i<10;i++)
+			System.out.print(ar[i]+" ");
+	
+	}
+
+
+
+}
