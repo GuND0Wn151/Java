4fd3960a0d0f3834eb3168f4cb9710ac8204efc0
GuND0Wn151 <kmaheshgupta151@outlook.com>
2020-12-21 17:49:58 +0530

    Create 24_TwoThreads2.java

---
+++ 24_TwoThreads2.java
@@ -0,0 +1,27 @@
+class MyThread extends Thread{
+	public void run(){
+		try{
+			for(int i=1;i<6;i++){
+			System.out.println(i);
+			Thread.sleep(2000);
+			}
+		}
+		catch (InterruptedException ie){}
+	}
+}
+
+
+public class MainThread4{
+public static void main(String[] ad){
+	MyThread t1=new MyThread();
+	try{	
+		t1.start();
+		t1.join();
+		for(int i=50;i<56;i++){	
+		System.out.println(i);
+		Thread.sleep(2000);
+		}
+		}
+	catch(Exception e){}
+	}
+}
