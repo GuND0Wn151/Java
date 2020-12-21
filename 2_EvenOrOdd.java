4a698063281e2fbacae17106e649b7b816af96ec
GuND0Wn151 <kmaheshgupta151@outlook.com>
2020-12-21 17:22:42 +0530

    Create 2_EvenOrOdd.java

---
+++ 2_EvenOrOdd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class EvenOrOdd{
+	public static void main(String args[]){
+		int a;
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the value");
+		a=input.nextInt();
+		if(a%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+
+}
