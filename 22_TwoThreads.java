22a2e59ec209b21ef499910ef4eade8dd7f4b606
GuND0Wn151 <kmaheshgupta151@outlook.com>
2020-12-21 17:49:54 +0530

    Create 22_TwoThreads.java

---
+++ 22_TwoThreads.java
@@ -0,0 +1,21 @@
+class SomeThread extends Thread{
+	public void run(){
+	try{
+	for(int i=1;i<6;i++){
+	System.out.println(i+" "+Thread.currentThread().getName());
+	Thread.sleep(2000);
+	}
+	}
+	catch(InterruptedException ie){}
+	}
+}
+public class MainThread2{
+public static void main(String[] asd){
+	SomeThread t1=new SomeThread();
+	SomeThread t2=new SomeThread();
+	t1.setName("Thread1");
+	t2.setName("Thread2");
+	t1.start();
+	t2.start();
+	}
+}
