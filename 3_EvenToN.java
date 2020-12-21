4499594496e16b1499084ae1e5d4e8775c88e463
GuND0Wn151 <kmaheshgupta151@outlook.com>
2020-12-21 17:22:40 +0530

    Create 3_EvenToN.java

---
+++ 3_EvenToN.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Task3{
+	public static void main(String args[]){
+		int a,i;
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the value");
+		a=input.nextInt();
+		for(i=1;i<=a;i++){
+			if(i%2==0)
+			System.out.println(i);
+		}
+	}
+}
