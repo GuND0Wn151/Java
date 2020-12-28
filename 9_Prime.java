bc3b1e8e40e865995cedac102f1895a880be2be5
GuND0Wn151 <kmaheshgupta151@outlook.com>
2020-12-28 15:10:45 +0530

    Update 9_Prime.java

--- 9_Prime.java
+++ 9_Prime.java
@@ -1,19 +1,22 @@
-import java.util.*;
-class Task9{
-	public static void main(String args[]){
-		int k=0,a;
+
+public class 9_Prime
+{
+	public static void main(String[] args) {
+		int k=0,a,b;
 		Scanner input =  new Scanner(System.in);
 		System.out.println("enter the value");
 		a=input.nextInt();
-		for(int i=2;i<a;i++){
-			if(a%i==0){
-				k+=1;
-				break;
+		b=input.nextInt();
+		for(int j =a;j<=b;j++){
+		    k=0;
+			for(int i=2;i<j;i++){
+				if(j%i==0)
+					k+=1;
 			}
+			if(k==0)
+			System.out.println(j);
+		
 		}
-		if(k==0)
-		System.out.println("PRime");
-		else
-		System.out.println("Not Prime");
+	
 	}
 }
